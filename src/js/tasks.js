c19c6cc0cfc132b18b37f9da2140bd68d8cf59b8
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2021-08-22 19:47:51 -0300

    Creating the Task Component and task listing

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -8,11 +8,33 @@ export default class Tasks {
 
   createNewTask(event) {
     if (event.keyCode === 13) {
-      let taskDescription = this.inputTask.value
-      let isCompleted = this.completed.checked
+      if (this.inputTask.value !== '') {
+        let taskDescription = this.inputTask.value
+        let isCompleted = this.completed.checked
 
-      console.log(taskDescription, isCompleted)
+        this.createTaskComponent(taskDescription, isCompleted)
+
+        this.inputTask.value = ''
+        this.completed.checked = false
+      }
     }
+  }
+
+  createTaskComponent(taskDescription, isCompleted) {
+    const taskContainer = document.querySelector('.tasks-container')
+
+    const taskItem = document.createElement('li')
+    const taskValue = document.createElement('span')
+    const taskStatus = document.createElement('input')
+    taskStatus.type = 'checkbox'
+
+    taskItem.classList.add('task-item')
+
+    taskValue.textContent = taskDescription
+    taskStatus.checked = isCompleted
+
+    taskContainer.appendChild(taskItem).appendChild(taskStatus)
+    taskItem.appendChild(taskValue)
   }
 
   addEvents() {
